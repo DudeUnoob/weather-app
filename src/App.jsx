48ba3da0191329ffca5e71c7772c9a8bedeaef06
Damodar Kamani <44341783+DudeUnoob@users.noreply.github.com>
2024-04-14 21:45:30 -0500

    navigation bar from reactbootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { WeatherProvider } from './context/WeatherContext';
+import NavigationBar from './components/NavigationBar';
+
 
 function App() {
   return (
     <BrowserRouter>
+    <NavigationBar />
     <WeatherProvider>
       <Routes>
         <Route path="/" element={<Home />} />
